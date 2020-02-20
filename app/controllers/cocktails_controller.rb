b9651580dbeae0c8e6b838c99129dd6ab2821e89
Alan Ruiz <gheryk90@gmail.com>
2020-02-20 17:14:50 +0100

    needs styling

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,40 @@
+class CocktailsController < ApplicationController
+
+  before_action :set_cocktail, only: [:show, :edit]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktails = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to new_cocktail_dose_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks_serialized = open(url).read
+drinks = JSON.parse(drinks_serialized)
+drinks = drinks['drinks']
+drinks.each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
+
+Cocktail.create(name: 'pepere')
+Cocktail.create(name: 'Old Fashion')
+Cocktail.create(name: 'Mojito')
